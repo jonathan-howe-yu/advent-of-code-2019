2b8d416d7c744e6c00a83025b6c1d174ba1a963c
Jon <jonathan.howe.yu@gmail.com>
2019-12-03 17:08:11 +0900

    Made basic terminal interface

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,21 +1,9 @@
 import { runProgram } from "./computer";
-import { readFile } from "fs";
 
 function fixProgram(program: number[], noun: number, verb: number): void {
   if (program.length < 3) throw new Error("Invalid program length");
   program[1] = noun;
   program[2] = verb;
-}
-
-function parseProgramFile(filename: string): Promise<number[]> {
-  return new Promise((resolve, reject) => {
-    readFile(filename, "utf8", (err, data: string) => {
-      if (err) reject(err);
-
-      // data is line-break-separated list of numbers
-      resolve(data.split(",").map(e => +e));
-    });
-  });
 }
 
 function solveForPos0(program: number[], noun: number, verb: number): number {
@@ -25,10 +13,7 @@ function solveForPos0(program: number[], noun: number, verb: number): number {
   return programClone[0];
 }
 
-function findSpecificPos0(
-  program: number[],
-  pos0Value: number
-): { noun: number; verb: number } {
+function findSpecificPos0(program: number[], pos0Value: number): { noun: number; verb: number } {
   for (let noun = 0; noun < 100; noun++) {
     for (let verb = 0; verb < 100; verb++) {
       if (solveForPos0(program, noun, verb) === pos0Value) {
@@ -40,20 +25,10 @@ function findSpecificPos0(
   throw new Error("Unable to find pos0Value within noun and verb range");
 }
 
-const args = process.argv.slice(2);
-const inputFile = args[0];
-if (!inputFile || typeof inputFile !== "string") {
-  throw new Error("Invalid input filename given as args");
+export default function solveDay2(program: number[]) {
+  const part1Solution = solveForPos0(program, 12, 2);
+  console.log(`Part 1 program data at pos 0: ${part1Solution}`);
+
+  const { noun, verb } = findSpecificPos0(program, 19690720);
+  console.log(`Part 2 solution: 100 * ${noun} + ${verb} = ${100 * noun + verb}`);
 }
-
-parseProgramFile(inputFile)
-  .then(program => {
-    const part1Solution = solveForPos0(program, 12, 2);
-    console.log(`Part 1 program data at pos 0: ${part1Solution}`);
-
-    const { noun, verb } = findSpecificPos0(program, 19690720);
-    console.log(
-      `Part 2 solution: 100 * ${noun} + ${verb} = ${100 * noun + verb}`
-    );
-  })
-  .catch(err => console.error(err));
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,71 @@
-// TODO: Create terminal program to run any day's code
+import readline from "readline";
+import { readFile } from "fs";
+import solveDay1 from "./day1";
+import solveDay2 from "./day2";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on("close", () => {
+  console.log("\nGood night!");
+  process.exit(0);
+});
+
+(function askWhatDay() {
+  setTimeout(
+    () =>
+      rl.question(
+        "What day do you want to see the solution for? [1-2] (Ctrl+C to close) ",
+        response => {
+          chooseDay(+response).then(
+            () => {
+              askWhatDay();
+              console.log("");
+            },
+            err => {
+              console.error(err);
+              rl.close();
+            }
+          );
+        }
+      ),
+    0
+  );
+})();
+
+function readFilePromise(filename: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    readFile(filename, "utf8", (err, data: string) => {
+      if (err) reject(err);
+
+      // data is line-break-separated list of numbers
+      resolve(data);
+    });
+  });
+}
+
+async function chooseDay(day: number): Promise<void> {
+  switch (day) {
+    case 1: {
+      const data = (await readFilePromise("./input/day1.txt"))
+        .split("\n")
+        .map(e => +e);
+
+      solveDay1(data);
+      return;
+    }
+
+    case 2: {
+      const data = (await readFilePromise("./input/day2.txt"))
+        .split(",")
+        .map(e => +e);
+
+      solveDay2(data);
+      return;
+    }
+    default:
+      console.log("Sorry, I can't do that...");
+  }
+}
